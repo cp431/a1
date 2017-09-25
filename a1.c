afe71b5843803967cc36297d978333157d6a2b62
Gareth <garethjsharpe@gmail.com>
2017-09-25 16:15:00 -0400

    Update a1.c

--- a1.c
+++ a1.c
@@ -1,8 +1,17 @@
 /*
  * a1.c
+ * MPI programming for gaps between prime numbers
  *
  *  Created on: Sep 25, 2017
- *      Author: shar1370
+ *     Authors: Gareth Sharpe (090361370),
+ *     			Other,
+ *     			Other,
+ *     			Other
+ *
  */
 
+#include <primesieve.h>
 
+int main(int arc, int *argv[]) {
+
+}
