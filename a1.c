5cd15a1a0555444dede56785e56839868a464b77
Gareth <garethjsharpe@gmail.com>
2017-10-02 16:45:17 -0400

    Lots of crap with MPI

--- a1.c
+++ a1.c
@@ -8,6 +8,11 @@
 
 #define FIRST 0
 
+// MPI Send/Receive Tag Names
+#define PRIME_INDEX_1 0
+#define PRIME_INDEX_2 1
+#define PRIME_GAP_STRING 2
+
 #include "mpi.h"
 #include <math.h>
 #include <stdio.h>
@@ -29,7 +34,7 @@ int main(int argc, char **argv)
   double start_time = 0;
   double end_time = 0;
   int p_rank = 0;
-  int tag = MPI_ANY_TAG;
+  int destination = 0;
   MPI_Status status;
   
   printf("Init prime list\n");
@@ -44,26 +49,44 @@ int main(int argc, char **argv)
   /******************** task with rank 0 does this part ********************/
   start_time = MPI_Wtime();   /* Initialize start time */
   if (p_rank == FIRST) {
-     mpz_t final;
-     mpz_init(final);
+     
+     int greatest_index_1 = 0;
+     int greatest_index_2 = 0;
+     char *greatest_prime_gap = "0";
      
      // Return largest prime gap from other processors
      for (int source = 1; source < processors; source++) { 
-        MPI_Recv(&max_diff, 1, MPI_LONG_LONG_INT, source, tag, MPI_COMM_WORLD, &status);
-          mpz_set_ui(final, max_diff);
+        int temp_index_1 = 0;
+        int temp_index_2 = 0;
+        char *temp_prime_gap;
+        
+        MPI_Recv(temp_index1, 1, MPI_LONG_LONG_INT, source, PRIME_INDEX_1, MPI_COMM_WORLD, &status);
+        MPI_Recv(temp_index2, 1, MPI_LONG_LONG_INT, source, PRIME_INDEX_1, MPI_COMM_WORLD, &status);
+        MPI_Recv(temp_prime_gap, 1, MPI_CHAR, source, PRIME_INDEX_1, MPI_COMM_WORLD, &status);
+        
+        if (strcmp(temp_prime, greatest_prime_gap) == 1) {
+           greates_prime_gap = temp_prime_gap;
+           greatest_index_1 = temp_index_1;
+           greatest_index_2 = temp_index_2;
+        }
+     printf("Largest gap %s", greatest_prime_gap);
      }
      
      end_time = MPI_Wtime();
-     printf("Wallclock time elapsed: %.2lf seconds\n", end_time-start_time);
+     printf("Wallclock time elapsed: %.2lf seconds\n", end_time - start_time);
   }
    
   /******************** all other tasks do this part ***********************/
   if (p_rank > FIRST) {
     /******************** split up array for load balancing ********************/
-  	 int evaluate_length = 0, list_length = 0, processors = 0, i_start = 0, j = 0; 
+  	 int evaluate_length = 0, list_length = 0, processors = 0, i_start = 0, j = 0;
     mpz_t max_diff, diff;
     mpz_init(max_diff);
     mpz_init(diff);
+     
+    int prime1 = 0;
+    int prime2 = 0;
+    char *max_diff_str = NULL;
 
   	evaluate_length = floor(list_length / processors);
   	if (p_rank < list_length % processors)
@@ -76,7 +99,15 @@ int main(int argc, char **argv)
   		if (mpz_cmp(diff, max_diff) == 1)
   			mpz_set(max_diff, diff);
     }
-
+    
+    int base = 10;
+    mpz_get_str(max_diff_str, base, max_diff);
+     
+    int count = 1;
+    int len = str_len(max_diff_str);
+    MPI_Send(&prime1, count, MPI_LONG_LONG_INT, destination, PRIME_INDEX_1, MPI_COMM_WORLD);
+    MPI_Send(&prime2, count, MPI_LONG_LONG_INT, destination, PRIME_INDEX_2, MPI_COMM_WORLD);
+    MPI_Send(max_diff_str, count, MPI_CHAR, destination, PRIME_GAP_STRING, MPI_COMM_WORLD);
     MPI_Finalize();
   }
 
