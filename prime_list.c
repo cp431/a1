c5ab33e217eab3a9814198560cf391f7b42dabf4
Gareth <garethjsharpe@gmail.com>
2017-10-05 11:47:27 -0400

    Update prime_list.c

--- prime_list.c
+++ prime_list.c
@@ -21,7 +21,7 @@ void init_prime_list(long long int *list, const long long int *problem_size)
 		mpz_set(temp, previous_prime);
 		gmp_printf("Prime %lld: %Zd\n", prime_count, temp);
 		prime = mpz_get_si(temp);
-		printf("%d\n", prime);
+		printf("%s\n", prime);
 		//printf("Prime %lld: %lld", prime_count, prime);
 		list[prime_count] = prime;
 		
