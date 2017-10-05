14b4b190fb765172e5f705b5c4f10ab972d80a00
Gareth <garethjsharpe@gmail.com>
2017-10-04 20:21:41 -0400

    Update a1.c

--- a1.c
+++ a1.c
@@ -114,7 +114,7 @@ int main(int argc, char **argv)
    // testing prime list
    printf("Printing prime list for process %d\n", p_rank);
    for (long long int i = 0; i < evaluate_length; i++) {
-      printf("%Zd ", &(get_prime_list_element_at(&list, &i)));
+      printf("%Zd ", *(get_prime_list_element_at(&list, &i)));
    }
    printf("\n");
     
