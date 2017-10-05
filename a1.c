2aa5b7b0f602532427270086a7e451f2ea82da62
Gareth <garethjsharpe@gmail.com>
2017-10-05 13:49:06 -0400

    Update a1.c

--- a1.c
+++ a1.c
@@ -65,7 +65,7 @@ int main(int argc, char **argv)
      }
      printf("\n");
      
-     printf("PRIMES ADDRESS: %d", &primes);
+     printf("PRIMES ADDRESS: %d", primes);
      for (int dest = 1; dest < num_processors; dest++) {
        MPI_Send(primes, COUNT, MPI_INT, dest, 0, MPI_COMM_WORLD);
      }
@@ -79,7 +79,7 @@ int main(int argc, char **argv)
     if (p_rank > FIRST) {
        MPI_Recv(primes, COUNT, MPI_INT, FIRST, 0, MPI_COMM_WORLD, &status);
        printf("Process %d got primes!\n", p_rank);
-       printf("PRIMES ADDRESS: %d", &primes);
+       printf("PRIMES ADDRESS: %d", primes);
     }
        
     /******************** split up array for load balancing ********************/
