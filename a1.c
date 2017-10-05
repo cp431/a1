e01ac880954f9865e890123f5b436bd7fde96d0a
Gareth <garethjsharpe@gmail.com>
2017-10-05 12:37:16 -0400

    Update a1.c

--- a1.c
+++ a1.c
@@ -37,7 +37,6 @@ int main(int argc, char **argv)
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &p_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &num_processors);
-  num_processors--;
   
   long long int temp_prime_1 = 0LL;
   long long int temp_prime_2 = 0LL;
