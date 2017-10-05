85f18afc99b0f468219273055cad244037e253a0
Gareth <garethjsharpe@gmail.com>
2017-10-05 17:19:13 -0400

    Update a1.c

--- a1.c
+++ a1.c
@@ -88,7 +88,7 @@ int main(int argc, char **argv)
     if (p_rank < problem_size % num_processors)
   			evaluate_length += 1;
    
-    int other_bit = min(p_rank, problem_size % num_processors);
+    int other_bit = MIN(p_rank, problem_size % num_processors);
 
   	 i_start = p_rank * evaluate_length + other_bit;
    evaluate_length++;
