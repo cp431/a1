d6bb6bb792c5b059081850961664f1864f525903
BradleyKatz <brad.katz13@gmail.com>
2017-10-04 23:18:05 -0400

    Update prime_list.c

--- prime_list.c
+++ prime_list.c
@@ -16,7 +16,7 @@ void init_prime_list(long long int *list, const long long int *problem_size)
 	{
 		// Add another prime number to the list.
 		//mpz_export(&list[prime_count], NULL, -1, sizeof(long long int), -1, 0, previous_prime);
-		list[primt_count] = mpz_get_si(previous_prime);
+		list[prime_count] = mpz_get_si(previous_prime);
 		
 		// Determine the next prime number greater than the last prime added to the list.
 		mpz_nextprime(next_prime, previous_prime);
