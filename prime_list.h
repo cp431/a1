036fbae3dbde832e859680711cda71de503a0808
BradleyKatz <brad.katz13@gmail.com>
2017-10-04 22:42:04 -0400

    Update prime_list.h

--- prime_list.h
+++ prime_list.h
@@ -3,54 +3,20 @@
 
 #include <gmp.h>
 
-// The exact value of this constant will be tweaked as we continue development.
-#define INIT_LIST_LENGTH 10000LL;
-
-// Typedefs
-typedef struct 
-{
-	long long int *capacity;
-	long long int *used;
-	long long int *values;
-} prime_list;
-
 // prime_list functions
 
 /**
-* Initializes a given prime_list to hold the first problem_size prime numbers.
+* Initializes a given array of long long ints to hold the first problem_size prime numbers.
 *
 * @param list pointer to the list of prime numbers.
-* @param upper_prime_bound the upper bound for prime number generation.
+* @param problem_size the amount of prime numbers to generate.
 */
-void init_prime_list(prime_list *list, const long long int *problem_size);
-
-/**
-* Returns the element at a given index.
-*
-* @param list pointer to the list of prime numbers.
-* @param index pointer to the index of the desired list element.
-* @return pointer to the element of list at index.
-*/
-inline long long int* get_prime_list_element_at(const prime_list *list, const long long int *index)
-{
-  return &(list->values[*index]);
-}
-
-/**
-* Returns the length of the list.
-*
-* @param list pointer to the list of prime numbers
-* @return the length of the prime_list this function was given
-*/
-inline const long long int* get_prime_list_length(const prime_list *list)
-{
-  return list->used;
-}
+void init_prime_list(long long int **list, const long long int *problem_size);
 
 /**
 * Frees all memory taken by the prime list and sets capacity and used back to 0.
 * 
 * @param list pointer to the list of prime numbers.
 */
-void clear_prime_list(prime_list *list);
+void clear_prime_list(long long int **list);
 #endif
