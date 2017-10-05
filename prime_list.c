58167829dad400306c569ec99a913274c10b4fdb
Gareth <garethjsharpe@gmail.com>
2017-10-05 12:02:25 -0400

    Update prime_list.c

--- prime_list.c
+++ prime_list.c
@@ -14,8 +14,8 @@ void init_prime_list(long long int *list, const long long int *problem_size)
 	mpz_init_set_si(next_prime, 0);
 	
 	// mpz_cmp returns a positive int if next_prime > upper_bound, 0 if =, negative if <
-	for (long long int prime_count = 0LL; prime_count < *problem_size; ++prime_count)
-	{
+	//for (long long int prime_count = 0LL; prime_count < *problem_size; ++prime_count)
+	//{
 		// Add another prime number to the list.
 		//mpz_export(&list[prime_count], NULL, -1, sizeof(long long int), -1, 0, previous_prime);
 		//mpz_set(temp, previous_prime);
@@ -24,24 +24,24 @@ void init_prime_list(long long int *list, const long long int *problem_size)
 		//printf("%ld\n", prime);
 		//printf("Prime %lld: %lld", prime_count, prime);
 		
-		long long int i = 3, c;
-		list[0] = 2LL;
-		for (long long int count = 2; count < problem_size; ) {
-		   for (c = 2 ; c <= i - 1 ; c++) {
-			if (i % c == 0)
-			   break;
-		      }  
-		   if (c == i) {
-		   	list[count] = i;
-			count++;
-		   }
-		   i++;
-		 }
+	long long int i = 3, c;
+	list[0] = 2LL;
+	for (long long int count = 2; count < *problem_size; ) {
+	   for (c = 2 ; c <= i - 1 ; c++) {
+		if (i % c == 0)
+		   break;
+	      }  
+	   if (c == i) {
+		list[count] = i;
+		count++;
+	   }
+	   i++;
+	 }
 		
 		// Determine the next prime number greater than the last prime added to the list.
 		//mpz_nextprime(next_prime, previous_prime);
 		//mpz_set(previous_prime, next_prime);
-	}
+	//}
 
 	mpz_clear(previous_prime);
 	mpz_clear(next_prime);
