4a19ceb6ffb3bc5e677049af0bd8a4db1907e2f5
Gareth <garethjsharpe@gmail.com>
2017-10-05 18:12:33 -0400

    Update a1.c

--- a1.c
+++ a1.c
@@ -136,6 +136,8 @@ int main(int argc, char **argv)
     MPI_Send(&temp_prime_2, COUNT, MPI_LONG_LONG_INT, FIRST, PRIME2, MPI_COMM_WORLD);
     MPI_Send(&temp_prime_gap, COUNT, MPI_LONG_LONG_INT, FIRST, PRIME_GAP, MPI_COMM_WORLD);
    
+   MPI_Barrier(MPI_COMM_WORLD);
+   
    if (p_rank == FIRST) {
            // Return largest prime gap from other processors
      for (int source = 1; source < num_processors; ++source) { 
