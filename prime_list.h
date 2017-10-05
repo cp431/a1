f6ab7504158f479a02b8370f0d98ad891f1fc406
BradleyKatz <brad.katz13@gmail.com>
2017-10-04 22:28:28 -0400

    long long int version

--- prime_list.h
+++ prime_list.h
@@ -11,18 +11,18 @@ typedef struct
 {
 	long long int *capacity;
 	long long int *used;
-	mpz_t *values;
+	long long int *values;
 } prime_list;
 
 // prime_list functions
 
 /**
-* Initializes a given prime_list to hold the first problem_size prime numbers after at and after starting_point.
+* Initializes a given prime_list to hold the first problem_size prime numbers.
 *
 * @param list pointer to the list of prime numbers.
 * @param upper_prime_bound the upper bound for prime number generation.
 */
-void init_prime_list(prime_list *list, const long long int *starting_point, const long long int *problem_size);
+void init_prime_list(prime_list *list, const long long int *problem_size);
 
 /**
 * Returns the element at a given index.
@@ -31,7 +31,7 @@ void init_prime_list(prime_list *list, const long long int *starting_point, cons
 * @param index pointer to the index of the desired list element.
 * @return pointer to the element of list at index.
 */
-inline mpz_t* get_prime_list_element_at(const prime_list *list, const long long int *index)
+inline long long int* get_prime_list_element_at(const prime_list *list, const long long int *index)
 {
   return &(list->values[*index]);
 }
@@ -45,18 +45,6 @@ inline mpz_t* get_prime_list_element_at(const prime_list *list, const long long 
 inline const long long int* get_prime_list_length(const prime_list *list)
 {
   return list->used;
-}
-
-/**
-* Subtracts num1 and num2 and stores the result in out_result.
-*
-* @param num1 pointer to the first operand
-* @param num2 pointer to the second operand
-* @param out_result pointer to the result of the subtraction
-*/
-inline void subtract_primes(const mpz_t num1, const mpz_t num2, mpz_t out_result)
-{
-	mpz_sub(out_result, num1, num2);
 }
 
 /**
