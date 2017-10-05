a090194502d0f79c877723937bae8342792c7be5
Gareth <garethjsharpe@gmail.com>
2017-10-05 11:10:44 -0400

    Update prime_list.c

--- prime_list.c
+++ prime_list.c
@@ -8,11 +8,11 @@ void init_prime_list(long long int *list, const long long int *problem_size)
 	mpz_t previous_prime, next_prime;
 
 	// The LL suffix specifies the this literal is a long long. Avoids implicit typecasting.
-	mpz_init_set_si(previous_prime, 1LL);
-	mpz_init_set_si(next_prime, 1LL);
+	mpz_init_set_si(previous_prime, 2LL);
+	mpz_init_set_si(next_prime, 0LL);
 	
 	// mpz_cmp returns a positive int if next_prime > upper_bound, 0 if =, negative if <
-	for (long long int prime_count = 0; prime_count < *problem_size; ++prime_count)
+	for (long long int prime_count = 0LL; prime_count < *problem_size; ++prime_count)
 	{
 		// Add another prime number to the list.
 		//mpz_export(&list[prime_count], NULL, -1, sizeof(long long int), -1, 0, previous_prime);
