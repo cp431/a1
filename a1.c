89f8640c552a0e0f71f3966921e41aa959339d70
Gareth <garethjsharpe@gmail.com>
2017-10-04 20:18:08 -0400

    Update a1.c

--- a1.c
+++ a1.c
@@ -73,9 +73,9 @@ int main(int argc, char **argv)
         MPI_Recv(temp_prime_2, BUFF, MPI_CHAR, source, PRIME2, MPI_COMM_WORLD, &status);
         MPI_Recv(temp_prime_gap, BUFF, MPI_CHAR, source, PRIME_GAP, MPI_COMM_WORLD, &status);
         
-        printf("temp_prime_gap_mpz @ process %d: %Zd", source, temp_prime_gap_mpz);
-        printf("greatest_prime_1 @ process %d: %Zd", source, greatest_prime_1);
-        printf("greatest_prime_2 @ process %d: %Zd", source, greatest_prime_2);
+        printf("temp_prime_gap_mpz @ process %d: %Zd\n", source, temp_prime_gap_mpz);
+        printf("greatest_prime_1 @ process %d: %Zd\n", source, greatest_prime_1);
+        printf("greatest_prime_2 @ process %d: %Zd\n", source, greatest_prime_2);
         
         mpz_set_str(temp_prime_gap_mpz, temp_prime_gap, BASE_DECIMAL);
         
