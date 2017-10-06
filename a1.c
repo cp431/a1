c1cba5de82d2b2762ba5f92afef22af19b6e65c5
Gareth <garethjsharpe@gmail.com>
2017-10-06 11:05:10 -0400

    Update documentation

--- a1.c
+++ a1.c
@@ -1,7 +1,11 @@
 /**
    a1.c
-   ====
-   This program will eventually do things.
+   MPI programming for gaps between prime numbers
+   Contributors:
+   -> Frank Khalil	160226600
+   -> Sarah Johnston	150139570
+   -> Brad Katz		130750210
+   -> Gareth Sharpe	090361370
 */
 
 // Include statment(s)
@@ -66,93 +70,100 @@ int main(int argc, char **argv) {
 
   // Initialize start time
   start_time = MPI_Wtime();   
-   
-  
-       
-/******************** split up array for load balancing ********************/
-long long int evaluate_length = 0LL, i_start = 0LL;
 
-evaluate_length = floor(problem_size / num_processors);
+  /******************** Split up array for load balancing ********************/
+  long long int evaluate_length = 0LL, i_start = 0LL;
 
-if (p_rank < problem_size % num_processors)
-		evaluate_length += 1;
+  evaluate_length = floor(problem_size / num_processors);
 
-int other_bit = MIN(p_rank, problem_size % num_processors);
+  if (p_rank < problem_size % num_processors)
+  		evaluate_length += 1;
 
-i_start = p_rank * evaluate_length + other_bit;
+  int remainder = MIN(p_rank, problem_size % num_processors);
 
-if (p_rank != num_processors - 1) {
-evaluate_length++;
-}
-   
-   mpz_t previous_prime, next_prime, diff, max_diff, prime1, prime2, end_prime;
-   mpz_init_set_si(previous_prime, i_start);
-   mpz_init_set_si(next_prime, i_start);
-   mpz_init_set_si(end_prime, evaluate_length + i_start);
-	
-   mpz_init(diff);
-   mpz_init(max_diff);
-   mpz_init(prime1);
-   mpz_init(prime2);
-	
-   mpz_nextprime(next_prime, previous_prime);
-   mpz_nextprime(end_prime, end_prime);
-	
-   gmp_printf("START for %d: %Zd, END: %Zd\n", p_rank, next_prime, end_prime);
-   //printf("Doot Doot! Process %d here, starting to compare primes!\n", p_rank); 
-   //for (long long int i = i_start; i < i_start + evaluate_length; ++i) {
-   while (mpz_cmp(end_prime, next_prime) == 1) {
-	 
-	 mpz_set(previous_prime, next_prime);
+  i_start = p_rank * evaluate_length + remainder;
 
-       
-         mpz_nextprime(next_prime, previous_prime);
-         mpz_sub(diff, next_prime, previous_prime);
-//          gmp_printf("%Zd ", previous_prime);
-//          gmp_printf("Prime 1 for p%d: %Zd\n", p_rank, previous_prime);
-//          gmp_printf("Prime 2 for p%d: %Zd\n", p_rank, next_prime);
-//          gmp_printf("Difference for p%d: %Zd\n", p_rank, diff);
-  		
-         if (mpz_cmp(diff, max_diff) == 1) {
-            mpz_set(max_diff, diff);
-            mpz_set(prime1, previous_prime);
-            mpz_set(prime2, next_prime);
-         }
-// 	 if (i == i_start)
-// 		 gmp_printf("FIRST prime for %d: %Zd\n", p_rank,previous_prime);
-//    	 else if (i == i_start + evaluate_length - 1)
-// 		gmp_printf("LAST prime for %d: %Zd\n", p_rank,previous_prime);
-    }
-   
-    mpz_export(&temp_prime_1, 0, -1, sizeof(long long int), 0, 0, prime1);
-    mpz_export(&temp_prime_2, 0, -1, sizeof(long long int), 0, 0, prime2);
-    mpz_export(&temp_prime_gap, 0, -1, sizeof(long long int), 0, 0, max_diff);
-                
-    MPI_Send(&temp_prime_1, COUNT, MPI_LONG_LONG_INT, FIRST, PRIME1, MPI_COMM_WORLD);
-    MPI_Send(&temp_prime_2, COUNT, MPI_LONG_LONG_INT, FIRST, PRIME2, MPI_COMM_WORLD);
-    MPI_Send(&temp_prime_gap, COUNT, MPI_LONG_LONG_INT, FIRST, PRIME_GAP, MPI_COMM_WORLD);
-   
-    if (p_rank == FIRST) {
-           // Return largest prime gap from other processors
-        for (int source = 0; source < num_processors; ++source) { 
-
-        MPI_Recv(&temp_prime_1, COUNT, MPI_LONG_LONG_INT, source, PRIME1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&temp_prime_2, COUNT, MPI_LONG_LONG_INT, source, PRIME2, MPI_COMM_WORLD, &status);
-        MPI_Recv(&temp_prime_gap, COUNT, MPI_LONG_LONG_INT, source, PRIME_GAP, MPI_COMM_WORLD, &status);
-        
-        if (temp_prime_gap > greatest_prime_gap) {
-           greatest_prime_gap = temp_prime_gap;
-           greatest_prime_1 = temp_prime_1;
-           greatest_prime_2 = temp_prime_2;
-        }
-     }
-
-     end_time = MPI_Wtime();
-     printf("\nWallclock time elapsed: %.2lf seconds\n", end_time - start_time);
-     printf("The largest prime gap is: %lld\n", greatest_prime_gap);
-     printf("This gap is realized by the difference between %lld and %lld\n", greatest_prime_1, greatest_prime_2); 
+  if (p_rank != num_processors - 1) {
+  evaluate_length++;
   }
-   
+
+  /******************** Initialize and/or set mpz_t variables ********************/
+
+  mpz_t previous_prime, next_prime, diff, max_diff, prime1, prime2, end_prime;
+
+  // Initialize solution storage variables
+  mpz_init(diff);
+  mpz_init(max_diff);
+  mpz_init(prime1);
+  mpz_init(prime2);
+
+  // Initialize and set preavious, next and end primes to initial indicies 
+  mpz_init_set_si(previous_prime, i_start);
+  mpz_init_set_si(next_prime, i_start);
+  mpz_init_set_si(end_prime, evaluate_length + i_start);
+
+  // Get the next prime beyond start/end indicies
+  mpz_nextprime(next_prime, previous_prime);
+  mpz_nextprime(end_prime, end_prime);
+
+  // Loop through all primes between the starting prime and the ending prime
+  while (mpz_cmp(end_prime, next_prime) == 1) {
+    
+    // Set up for mpz_nextprime by shifting next prime to previous prime
+    mpz_set(previous_prime, next_prime);
+    // Get the next prime past our previous prime
+    mpz_nextprime(next_prime, previous_prime);
+    // Determine the difference between the primes by subtracting them
+    mpz_sub(diff, next_prime, previous_prime);
+
+    // If the difference found is greater than the current max difference, update max_diff 
+    if (mpz_cmp(diff, max_diff) == 1) {
+      mpz_set(max_diff, diff);
+      mpz_set(prime1, previous_prime);
+      mpz_set(prime2, next_prime);
+    }
+  }
+
+  // Export each processors greatest found prime gap, as well as the two primes that created the gap
+  mpz_export(&temp_prime_1, 0, -1, sizeof(long long int), 0, 0, prime1);
+  mpz_export(&temp_prime_2, 0, -1, sizeof(long long int), 0, 0, prime2);
+  mpz_export(&temp_prime_gap, 0, -1, sizeof(long long int), 0, 0, max_diff);
+
+  // Send the data to process zero            
+  MPI_Send(&temp_prime_1, COUNT, MPI_LONG_LONG_INT, FIRST, PRIME1, MPI_COMM_WORLD);
+  MPI_Send(&temp_prime_2, COUNT, MPI_LONG_LONG_INT, FIRST, PRIME2, MPI_COMM_WORLD);
+  MPI_Send(&temp_prime_gap, COUNT, MPI_LONG_LONG_INT, FIRST, PRIME_GAP, MPI_COMM_WORLD);
+
+  /******************** Only process 0 completes this task ********************/
+  if (p_rank == FIRST) {
+
+    // Loop through each source processor 
+    for (int source = 0; source < num_processors; ++source) { 
+
+      // Recieve each processors greatest found prime gap, as well as the two primes that created the gap
+      MPI_Recv(&temp_prime_1, COUNT, MPI_LONG_LONG_INT, source, PRIME1, MPI_COMM_WORLD, &status);
+      MPI_Recv(&temp_prime_2, COUNT, MPI_LONG_LONG_INT, source, PRIME2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&temp_prime_gap, COUNT, MPI_LONG_LONG_INT, source, PRIME_GAP, MPI_COMM_WORLD, &status);
+
+      // Determine the greatest prime gap from amongst all the processors and update solution variables
+      if (temp_prime_gap > greatest_prime_gap) {
+         greatest_prime_gap = temp_prime_gap;
+         greatest_prime_1 = temp_prime_1;
+         greatest_prime_2 = temp_prime_2;
+      }
+    }
+
+    // Initialize end time
+    end_time = MPI_Wtime();
+
+    // Print computed elapsed time and solution
+    printf("\nWallclock time elapsed: %.2lf seconds\n", end_time - start_time);
+    printf("The largest prime gap is: %lld\n", greatest_prime_gap);
+    printf("This gap is realized by the difference between %lld and %lld\n", greatest_prime_1, greatest_prime_2); 
+  }
+
+  // Finalize the parallel process
   MPI_Finalize();
+  // Return exit success
   return 0;
 }
