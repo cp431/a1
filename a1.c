5a92c68ef86e983b3b86b95e1be1096982c44e83
Sjohnston3700 <31933443+Sjohnston3700@users.noreply.github.com>
2017-10-06 07:23:31 -0400

    Update a1.c

--- a1.c
+++ a1.c
@@ -85,18 +85,18 @@ int main(int argc, char **argv) {
    mpz_init(prime2);
    mpz_nextprime(next_prime, previous_prime);
    
-//    printf("Doot Doot! Process %d here, starting to compare primes!\n", p_rank); 
+   printf("Doot Doot! Process %d here, starting to compare primes!\n", p_rank); 
    for (long long int i = i_start; i < i_start + evaluate_length - 1; ++i) {
 	 
 	 mpz_set(previous_prime, next_prime);
-// 	 printf("i_start: %lld\n", i_start);
+	 printf("i_start: %lld\n", i_start);
        
          mpz_nextprime(next_prime, previous_prime);
          mpz_sub(diff, next_prime, previous_prime);
       
-//          gmp_printf("Prime 1: %Zd\n", previous_prime);
-//          gmp_printf("Prime 2: %Zd\n", next_prime);
-//          gmp_printf("Difference: %Zd\n", diff);
+         gmp_printf("Prime 1: %Zd\n", previous_prime);
+         gmp_printf("Prime 2: %Zd\n", next_prime);
+         gmp_printf("Difference: %Zd\n", diff);
   		
          if (mpz_cmp(diff, max_diff) == 1) {
             mpz_set(max_diff, diff);
